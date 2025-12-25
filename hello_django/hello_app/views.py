c4f3a68c9180665a6dd7ed706c47701c30f2333c
web development <vyshnavycm@gmail.com>
2025-12-25 14:15:24 +0530

    class 3 completed, html insertion is done

--- hello_django/hello_app/views.py
+++ hello_django/hello_app/views.py
@@ -1,6 +1,40 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import render
 
 # Create your views here.
 def print_hello(request):
-    return HttpResponse("hello django")
+    movie_data={
+        'movies':[
+       {
+        'title':'God father',
+        'year':'1999',
+        'author':'padmarajan',
+        'success':True
+       },
+       {
+        'title':'goldfish',
+        'year':'1990',
+        'success':True
+       },
+       {
+        'title':'great father',
+        'year':'1999',
+        'author':'padmarajan',
+        'success':True
+       }, 
+       {
+        'title':'bha ba bha',
+        'year':'1999',
+        'author':'padmarajan',
+        'success':True
+        }
+
+    ]}    
+
+
+
+
+    return render(request,'hello.html',movie_data)
+  
+
