624bbbbe5970757886e0ea0535f53fe21e1576af
web development <vyshnavycm@gmail.com>
2025-12-26 16:25:43 +0530

    class 5 completed,static page adding done

--- movie_manager/movies/views.py
+++ movie_manager/movies/views.py
@@ -3,10 +3,45 @@ from django.shortcuts import render
 # Create your views here.
 
 def create(request):
-    return render (request,'create.html')
+    if request.POST:
+        print(request.POST)
+    return render(request, 'create.html')
+
 
 def list(request):
-    return render (request,'list.html')
+    movie_data = {
+        'movies': [
+            {
+                'title': 'God father',
+                'year': '1999',
+                'author': 'padmarajan',
+                'success': True
+            },
+            {
+                'title': 'goldfish',
+                'year': '1990',
+                'author': 'unknown',
+                'success': True
+            },
+            {
+                'title': 'great father',
+                'year': '1999',
+                'author': 'padmarajan',
+                'success': True
+            },
+            {
+                'title': 'bha ba bha',
+                'year': '1999',
+                'author': 'padmarajan',
+                'success': True
+            }
+        ]
+    }
+
+    return render(request, 'list.html', movie_data)
+
 
 def edit(request):
-    return render (request,'edit.html')
+    return render(request, 'edit.html')
+
+
